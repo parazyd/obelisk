efdd930fa0b7808d663da689d4f0f08824f78c36
parazyd <parazyd@dyne.org>
2021-04-07 17:20:43 +0200

    Start adding some utility functions.

---
+++ electrumobelisk/util.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# Copyright (C) 2020-2021 Ivan J. <parazyd@dyne.org>
+#
+# This file is part of obelisk
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License version 3
+# as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""Utility functions"""
+
+
+def is_integer(val):
+    return isinstance(val, int)
+
+
+def is_non_negative_integer(val):
+    if is_integer(val):
+        return val >= 0
+    return False
